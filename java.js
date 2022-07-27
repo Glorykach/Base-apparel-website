f91fd324822a384d309be85db6c927535078d6a1
Onyeka Glory❤️ <88969626+Glorykach@users.noreply.github.com>
2022-07-27 01:10:27 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 function SUBMIT(){
     if (document.getElementById("EmailId").value==""){
-    document.getElementById("EmailId").style.background="url('./Images/icon-error.svg') 98% 40% no-repeat";
+    document.getElementById("EmailId").style.background="url('./images/icon-error.svg') 98% 40% no-repeat";
     document.getElementById("WarningText").style.display="block";
     document.getElementById("EmailId").style.border="1px solid red";
     
@@ -8,7 +8,7 @@ function SUBMIT(){
 }
 function Remove(){
     if (document.getElementById("EmailId").value!=""){
-        document.getElementById("EmailId").style.background="url('./Images/icon-error.svg') 380005px 10px no-repeat";
+        document.getElementById("EmailId").style.background="url('./images/icon-error.svg') 380005px 10px no-repeat";
         document.getElementById("WarningText").style.display="none";
         document.getElementById("EmailId").style.border="1px solid black";
         
@@ -16,7 +16,7 @@ function Remove(){
 }
 function SUBMIT2(){
     if (document.getElementById("EmailId").value==""){
-        document.getElementById("EmailId").style.background="url('./Images/icon-error.svg') 75% 50% no-repeat";
+        document.getElementById("EmailId").style.background="url('./images/icon-error.svg') 75% 50% no-repeat";
         document.getElementById("WarningText").style.display="block";
         document.getElementById("EmailId").style.border="1px solid #F96464";
         
